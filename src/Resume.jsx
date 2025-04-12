2b80d00df44542aae864cbdd8a4afbbe89a959e1
victorvladimirovich1 <jesin.tima.2006@gmail.com>
2025-04-12 16:33:09 +0600

    first_commit

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -34,7 +34,11 @@ const Resume = () => {
                 d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
               ></path>
             </svg>
-            <p>ул. Белмазар 18, Бишкек, Кыргызстан</p>
+            <p>
+              <a href="https://maps.app.goo.gl/YuhJCmWwM5fYSZwH6">
+                ул. Белмазар 18, Бишкек, Кыргызстан
+              </a>
+            </p>
           </div>
           <div className="flex items-center">
             <svg
@@ -51,7 +55,11 @@ const Resume = () => {
                 d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
               ></path>
             </svg>
-            <p>+996 995 997 344</p>
+            <p>
+              <a href="tel:+996995997344" className="text-gray-700">
+                <span>+996 995 997 344</span>
+              </a>
+            </p>
           </div>
           <div className="flex items-center">
             <svg
@@ -68,7 +76,11 @@ const Resume = () => {
                 d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
               ></path>
             </svg>
-            <p>jesin.tima.2006@gmail.com</p>
+            <p>
+              <a href="mailto:alievnurtilek538@gmail.com">
+                alievnurtilek538@gmail.com
+              </a>
+            </p>
           </div>
         </div>
 
